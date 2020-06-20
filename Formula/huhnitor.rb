a53f0e4b252fedee71263e5a8674076da0cd8a88
Stefan Kremser <github@spacehuhn.de>
2020-06-20 19:12:21 +0200

    Update huhnitor.rb

--- Formula/huhnitor.rb
+++ Formula/huhnitor.rb
@@ -4,8 +4,8 @@
 class Huhnitor < Formula
   desc "An intergalactic serial monitor for the ESP8266 Deauther"
   homepage "https://github.com/SpacehuhnTech/Huhnitor"
-  url "https://github.com/SpacehuhnTech/Huhnitor/releases/download/v1.1.0-rc1/huhnitor_1.1.0-rc1_mac.tar.gz"
-  sha256 "816cc62d0523b01e6e9d8c70e34fab6efd7a38c4d15bad5c44a731ed2dc80b43"
+  url "https://github.com/SpacehuhnTech/Huhnitor/releases/download/1.1.0/huhnitor_1.1.0_mac.tar.gz"
+  sha256 "6c1dc35a0bea49b98b6a4d336ecda2c24ef34d0fc7b0346ba72ee7bcffcaced3"
   version "1.1.0"
 
   def install
